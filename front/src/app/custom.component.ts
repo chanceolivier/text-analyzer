151776cfb59ee045eda70c97d616907959f949f3
Olivier Chancé <chanceolivier@gmail.com>
2017-07-25 15:10:18 +0200

    big commit

---
+++ front/src/app/custom.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom',
+  templateUrl: './custom.component.html',
+  styleUrls: ['./custom.component.css'],
+})
+export class CustomComponent { }
---
+++ front/src/app/presentation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'presentation',
+  template: `
+  <div>Je me présente, je m'appelle Olivier</div>
+  `
+})
+export class PresentationComponent { }
